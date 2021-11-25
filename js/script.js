2064eb94fbb694ceab4bc4936b5cddf455560f60
salvomel <salv.mel@gmail.com>
2021-11-25 15:32:48 +0100

    done

---
+++ js/script.js
@@ -0,0 +1,59 @@
+/*
+1. Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+2. Creare un array di oggetti di studenti.
+Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+3. Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+_______________________________________________________________________________________________________________________________________________________*/
+
+// ES. 1
+const studente = {
+    nome: 'Gianni',
+    cognome: 'Zanchi',
+    eta: 30
+};
+
+for ( let key in studente ) {
+    console.log ( studente[key] );
+}
+
+// ES. 2
+const studenti = [
+    {
+        nome: 'Luca',
+        cognome: 'Mandelli',
+        eta: 30
+    },
+    {
+        nome: 'Antonella',
+        cognome: 'Bruni',
+        eta: 25
+    },
+    {
+        nome: 'Mario',
+        cognome: 'Ferrari',
+        eta: 27
+    }
+];
+
+for ( i=0; i < studenti.length; i++) {
+    const theseStudents = studenti[i];
+      
+    for ( let key in theseStudents ) {
+        console.log ( theseStudents[key] );
+    }
+}
+
+// ES. 3 
+const newName = prompt ('Inserire nome studente');
+const newSurname = prompt ('Inserire cognome');
+const newAge = parseInt ( prompt ('Inserire età') );
+
+const newStudent = {
+    nome: newName,
+    cognome: newSurname,
+    eta: newAge
+}
+
+studenti.push(newStudent);
+console.log(studenti);
